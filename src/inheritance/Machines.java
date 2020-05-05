e44430a8932a578834fea0e04730a33b5c56d98b
saurabhpilankar <saurabhpilankar@gmail.com>
2020-05-05 15:54:12 +0530

    Second commit

--- src/inheritance/Machines.java
+++ src/inheritance/Machines.java
@@ -5,7 +5,9 @@ public class Machines {
 	 String name = "Machine Type : Petrol";
 
 	public void start() {
-	System.out.println("Machine started");	
+	System.out.println("Machine started");
+	System.out.println("Machine working");	
+
 	}
 	
 	public void stop() {
